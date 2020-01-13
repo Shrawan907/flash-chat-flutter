c08fa5d1421588334ea38fd6b5710b680858fe44
Shrawan907 <shrawan07090@gmail.com>
2020-01-14 00:28:51 +0530

    linking firebase

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -64,7 +64,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 try {
                   final newUser = await _auth.createUserWithEmailAndPassword(email: email, password: password);
                   if(newUser != null){
-                    Navigator.pushNamed(context, ChatScreen.id)
+                    Navigator.pushNamed(context, ChatScreen.id);
                   }
                 }
                 catch (e) {
